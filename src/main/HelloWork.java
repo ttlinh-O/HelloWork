858eeda798809b4ed38768dbb528a7c09355da82
ttlinh <linh.to@axonactive.com>
2022-01-08 16:24:26 +0700

    Add hello program

---
+++ src/main/HelloWork.java
@@ -0,0 +1,16 @@
+/*
+ * HelloWork.java
+ *
+ * Copyright by Axon Ivy (Lucerne), all rights reserved.
+ */
+
+package main;
+
+public class HelloWork {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello .....:" + i);
+        }
+    }
+}
